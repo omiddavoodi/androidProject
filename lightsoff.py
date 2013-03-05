d8d40ce7367a23a76d904990cec0f6150c3e1918
Omid Davoodi <ariestinak@gmail.com>
2013-03-05 13:16:50 +0330

    Bugfixes and cleanup
    Fixed the fourth map not being winnable
    Exit on winning last level
    Fixed the wrong orientation bug
    Changed the color of rectangles
    Cleanup and comments

--- lightsoff.py
+++ lightsoff.py
@@ -32,11 +32,11 @@ mapdata = [
     [0,0,1,0,0]
     ],
     [
-    [0,0,0,0,0],
+    [0,1,1,1,0],
     [0,1,0,1,0],
-    [0,0,0,0,0],
+    [1,0,1,0,1],
     [0,1,0,1,0],
-    [0,0,0,0,0]
+    [0,1,1,1,0]
     ]
     ]
 
@@ -44,55 +44,51 @@ class MyPaintWidget(Widget):
     
     rects = [] #this holds the state of 25 different areas
     started = False #determines whether the game is started or not
-    level = 0
+    level = 0 #current level
 
     def update_screen(self):
+        '''updated the screen'''
+        #we redraw everything at each update so that the game can support the rotation of the device
         with self.canvas:
+            #clear everthing
+            Color(0, 0, 0)
+            Rectangle(pos=self.pos, size=self.size)
             for i in range(5):
                 for j in range(5):
                     if (self.rects[i][j] == 1): #on
-                        Color(1, 0, 0)
+                        Color(0.5, 0.35, 0.95)
                     else: #off
-                        Color(1, 0, 1)
+                        Color(0.2, 0.2, 0.2)
                     #it will draw new rectangles over the older ones.
-                    #TODO: only draw the changed rectangles
                     Rectangle(pos=[self.pos[0] + 2 + 
                     (i * self.size[0] / 5.0), self.pos[1] + 2 + (j * self.size[1] / 5.0)]
                     , size=[self.size[0] / 5.0 - 4, self.size[1] / 5.0 - 4])
 
     def update_map(self, i, j):
         '''updates the map according to touch'''
-        print self.rects
-        self.rects[i][j] = 1 if not self.rects[i][j] else 0
-        if i - 1 >= 0 : self.rects[i - 1][j] = 1 if not self.rects[i - 1][j] else 0
-        if i + 1 <= 4 : self.rects[i + 1][j] = 1 if not self.rects[i + 1][j] else 0
-        if j - 1 >= 0 : self.rects[i][j - 1] = 1 if not self.rects[i][j - 1] else 0
-        if j + 1 <= 4 : self.rects[i][j + 1] = 1 if not self.rects[i][j + 1] else 0
-
+        self.rects[i][j] = not self.rects[i][j] #the touched tile...
+        if i - 1 >= 0 : self.rects[i - 1][j] = not self.rects[i - 1][j] # And
+        if i + 1 <= 4 : self.rects[i + 1][j] = not self.rects[i + 1][j] # the
+        if j - 1 >= 0 : self.rects[i][j - 1] = not self.rects[i][j - 1] # adjected
+        if j + 1 <= 4 : self.rects[i][j + 1] = not self.rects[i][j + 1] # tiles
+    
     def check_button(self, touch):
         '''finds out with button has been touched'''
-
+        #not the most accurate way but effective still
         i = int (touch.x / self.size[0] * 5.0)
         j = int (touch.y / self.size[1] * 5.0)        
         self.update_map(i, j)
 
     def load_level(self, level):
+        '''loads level data'''
         for i in range(5):
             self.rects.append([])
             for j in range(5):
-                self.rects[i].append(mapdata[level][i][j])
-        with self.canvas:
-            for i in range(5):
-                for j in range(5):
-                    if (mapdata[level][j][i] == 1):
-                        Color(1, 0, 0)
-                    else:
-                        Color(1, 0, 1)
-                    Rectangle(pos=[self.pos[0] + 2 + 
-                    (i * self.size[0] / 5.0), self.pos[1] + 2 + (j * self.size[1] / 5.0)]
-                    , size=[self.size[0] / 5.0 - 4, self.size[1] / 5.0 - 4])
-
+                self.rects[i].append(mapdata[level][j][i])
+        self.update_screen() #visualize the level after we loaded it
+        
     def next_level(self):
+        '''checks if the level is finished'''
         flag = True
         for i in self.rects:
             for j in i:
@@ -101,19 +97,23 @@ class MyPaintWidget(Widget):
         return flag
 
     def start(self, level):
-        #upon start, load the first level               
+        '''upon start, load the first level'''
         self.rects = []
         self.load_level(level)
                 
     def on_touch_down(self, touch):
-        #TODO: a splash screen should replace the black one. should be done in mypaint.kv
+        '''handles the touch event'''
+        #TODO: a splash screen should replace the black one. should be done in mypaint.kv --- Omid ---
 	if (not self.started):
 	    self.start(0)
 	    self.started = True
 	else: #only check buttons if we have really started the game.
             self.check_button(touch)
             self.update_screen()
-            if self.next_level():self.start(self.level)
+            if self.next_level():
+                if (self.level == len(mapdata)):
+                    exit() #TODO: some screen, etc. --- Unassigned --- maybe Reza?
+                self.start(self.level)
         
 class MyPaintApp(App):
     def build(self):
